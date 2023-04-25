0b986f8784d547e27082bdc4b1dc1c3bae92db35
Alejandro Salazar <alejandroslzr17@gmail.com>
2023-04-25 01:32:24 -0600

    locations on map with distance from center

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,30 +21,45 @@ function getAPI(url) {
 
   getAPI(url);
 */
-let APIroutes = "S4UzPeG7IlMqPo9isVNMWntTGfEr5s0Y"
-let urlRoutes;//"https://api.tomtom.com/map/1/sprite/20.0.0-8/metadata.json?key=S4UzPeG7IlMqPo9isVNMWntTGfEr5s0Y" 
-//urlRoutes="https://api.tomtom.com/search/2/nearbySearch/.json?key=S4UzPeG7IlMqPo9isVNMWntTGfEr5s0Y&lat=24.815&lon=-107.39"
 
+
+let APIroutes = "S4UzPeG7IlMqPo9isVNMWntTGfEr5s0Y"
+let urlRoutes;
+let urlRoutesAPI;
 let lat;
+let lat1
 let lon;
+let lon1;
+let center;
+let map;
+
 const options = {
   enableHighAccuracy: true,
-  timeout: 10000,
+  timeout: 15000,
   maximumAge: 0,
 };
 
 function success(pos) {
   const crd = pos.coords;
-  
-  console.log("Your current position is:");
-  console.log(`Latitude : ${crd.latitude}`);
-  console.log(`Longitude: ${crd.longitude}`);
-  console.log(crd.latitude)
+
   lat=crd.latitude;
   lon=crd.longitude;
-  urlRoutes= "https://api.tomtom.com/search/2/categorySearch/cinema.json?key=S4UzPeG7IlMqPo9isVNMWntTGfEr5s0Y&lat="+lat+"&lon="+lon;
+  urlRoutes= "https://api.tomtom.com/search/2/categorySearch/cinema.json?key=S4UzPeG7IlMqPo9isVNMWntTGfEr5s0Y&lat="+lat+"&lon="+lon+"&radius=4000";
   tomtomAPI(urlRoutes);
+  center = [lon,lat]
+  console.log(lon+","+lat+":52.50274,13.43872");
+  //urlRoutesAPI = "https://api.tomtom.com/routing/1/calculateRoute/"+lon+","+lat+":52.50274,13.43872/json?instructionsType=text&language=en-US&vehicleHeading=90&sectionType=traffic&report=effectiveSettings&routeType=eco&traffic=true&avoid=unpavedRoads&travelMode=car&vehicleMaxSpeed=120&vehicleCommercial=false&vehicleEngineType=combustion&key=IWlwFtnFKKA6voxvGd7uWEcCNXeuXtI4";
+  //tomtomRoutes(urlRoutesAPI);
+    map = tt.map({
+    key:APIroutes,
+    container:"map",
+    center: center,
+    zoom: 10
+  })
+
+ 
 }
+
 
 function error(err) {
   console.warn(`ERROR(${err.code}): ${err.message}`);
@@ -57,22 +72,35 @@ function tomtomAPI(urlRoutes) {
     fetch(urlRoutes)
       .then(function (response) {
         response.json().then(function (data){
+        console.log(data.results)
+        map.on('load', ()=>{
+          new tt.Marker().setLngLat(center).addTo(map)
+          for(let i =0; i<data.results.length;i++){
+            let name = data.results[i].poi.name
+            let name2= name.slice(0,10)
+            if(name2 !== 'Mm Cinemas'){
+              new tt.Popup().setLngLat([data.results[i].position.lon,data.results[i].position.lat+0.002]).setText(data.results[i].poi.name+' '+((data.results[i].dist)/1000).toFixed(2)+'Km').addTo(map)
+              new tt.Marker().setLngLat([data.results[i].position.lon,data.results[i].position.lat]).addTo(map)
+            }
+          }
+        })
+
+
+        })
+      });
+  }
+
+let routingAPIkey = 'IWlwFtnFKKA6voxvGd7uWEcCNXeuXtI4'
+
+  function tomtomRoutes(urlRoutesAPI) {
+    fetch(urlRoutesAPI)
+      .then(function (response) {
+        response.json().then(function (data){
 
         console.log(data)
         })
       });
   }
 
-  let center = [-107.39,24.815]
-  const map = tt.map({
-    key:APIroutes,
-    container:"map",
-    center: center,
-    zoom: 10
-  })
-  map.on('load', ()=>{
-    new tt.Marker().setLngLat(center).addTo(map)
-  })
 
-  console.log(window)
 
